5ececc59ab389371c44b59d67df95cd254beefcf
Irene-Recio <irene.recio-fernandez@etud.univ-pau.fr>
2026-03-06 14:24:26 +0100

    file
    Refactored File class to extend ChatItem and removed previous properties and methods.

--- File.java
+++ File.java
@@ -1,35 +1,3 @@
-public class File {
-    private String fileName;
-
-    private int fileSize;
-
-    private byte[] fileData;
-
-    public File(String fileName, int fileSize, byte[] fileData) {
-        this.fileName = fileName;
-        this.fileSize = fileSize;
-        this.fileData = fileData;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public int getFileSize() {
-        return fileSize;
-    }
-
-    public byte[] getFileData() {
-        return fileData;
-    }
-
-    @Override
-    public String toString() {
-        return "File{" +
-                "fileName='" + fileName + '\'' +
-                ", fileSize=" + fileSize +
-                '}';
-    }
-
+public class File extebnds ChatItem {
     
 }
