b195a09943fca43180bbeb5129af481b1b14f7e8
Irene-Recio <irene.recio-fernandez@etud.univ-pau.fr>
2026-03-06 14:55:59 +0100

    file class

--- File.java
+++ File.java
@@ -1,3 +1,28 @@
 public class File extends ChatItem {
+    private String fileName;
+
+    private long fileSize;
+
+    private byte[] fileContent;
+
+    public File(String id, Date timestamp, User sender, List<User> recipients, String fileName, long fileSize, byte[] fileContent) {
+        super(id, timestamp, sender, recipients);
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.fileContent = fileContent;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public byte[] getFileContent() {
+        return fileContent;
+    }
+
     
 }
