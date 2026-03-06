4192d1763812309762032ac9230a3ef85ed28cd8
Irene-Recio <irene.recio-fernandez@etud.univ-pau.fr>
2026-03-06 15:32:13 +0100

    ChatController mehtods

--- Controllers/ChatController.java
+++ Controllers/ChatController.java
@@ -1,2 +1,29 @@
 public class ChatController {
+
+    privaete List<ChatItem> chatItems;
+
+    public ChatController() {
+        this.chatItems = new ArrayList<>();
+    }
+
+    public void connect(username) {
+        User user = new User(randomId(), username);
+        // Notify UserController about the new connection
+        UserController.getInstance().notifyUserConnected(user);
+    }
+
+    public void disconnect(User user) {
+        // Notify UserController about the disconnection
+        UserController.getInstance().notifyUserDisconnected(user);
+    }
+
+    public void sendMessage(Message message, List<User> recipients) {
+    
+    }
+
+     public void sendFile(File file, List<User> recipients) {
+     }
+
+     public void receiveItem(ChatItem item) {
+     }
 }
