1093f34f29d2c0e37c38d467df8b6a47becc7ab1
Irene-Recio <irene.recio-fernandez@etud.univ-pau.fr>
2026-03-13 09:08:24 +0100

    main

--- main.java
+++ main.java
@@ -1,8 +1,13 @@
-public class Main extends Thread {
+// Main class is the main code that creates and runs the chat system
+
+public class Main {
     public static void main(String[] args) {
         ChatController chatController = new ChatController();
         chatController.start();
-        
+
+            UserListView userListView = new UserListView();
+            NotificationView notificationView = new NotificationView();
+            
 
     }
 
