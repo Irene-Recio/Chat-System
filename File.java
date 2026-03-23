a7d87926c81e3b9688735fd9ace14c57f6b6bf2f
Irene-Recio <irene.recio-fernandez@etud.univ-pau.fr>
2026-03-23 15:23:24 +0100

    File.java file

--- File.java
+++ File.java
@@ -1,3 +1,42 @@
 public class File {
-    
+    private String filename;
+    private byte[] content;
+    private long size;
+
+    public File(String filename, byte[] content) {
+        this.filename = filename;
+        this.content = content;
+        this.size = content.length;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String toString() {
+        return "File{" +
+                "filename='" + filename + '\'' +
+                ", size=" + size +
+                '}';
+    }
 }
